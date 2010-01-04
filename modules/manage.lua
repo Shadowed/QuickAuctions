532f89349f1d21d63dba6f0a04bbaac0773048a7
Shadowed <shadowed.wow@gmail.com>
2010-01-04 12:58:41 -0800

    Wheeeeeeeeeeee

--- modules/manage.lua
+++ modules/manage.lua
@@ -411,10 +411,11 @@ function Manage:Cancel(isTest)
 				local undercutBuyout, undercutBid, undercutOwner
 				if( QuickAuctions.db.factionrealm.player[lowestOwner] ) then
 					undercutBuyout, undercutBid, undercutOwner = QuickAuctions.Scan:GetSecondLowest(itemID, lowestBuyout)
-					undercutBuyout = undercutBuyout or lowestBuyout
-					undercutBid = undercutBid or lowestBid
-					undercutOwner = undercutOwner or lowestOwner
 				end
+
+				undercutBuyout = undercutBuyout or lowestBuyout
+				undercutBid = undercutBid or lowestBid
+				undercutOwner = undercutOwner or lowestOwner
 				
 				-- Don't cancel if the buyout is equal, or below our threshold
 				if( QuickAuctions.db.global.smartCancel and lowestBuyout <= threshold and not QuickAuctions.Scan:IsPlayerOnly(itemID)) then
